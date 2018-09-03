2d321c46f75ffb06fad6b7e0598c3c52a7be56f0
Alex Harlamov <halex.alex@yandex.ru>
2018-09-04 00:20:28 +0300

    Initial commit

---
+++ vue-skills/src/components/About.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="about">
+        <h1>Hello {{ their_name }}, This is about page</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus dignissimos aspernatur dolor nam placeat hic, aliquam quae provident recusandae saepe, et animi inventore eos facilis ratione quaerat non. Voluptatum, ut.</p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Skills',
+  data() {
+    return {
+      their_name: this.$route.params.name
+    }
+  },
+}
+</script>
+
